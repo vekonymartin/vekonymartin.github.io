c1e479187185d2e5161b86f6f317bce12b61b36b
vekonymartin <vekony.martin15@gmail.com>
2020-01-27 18:12:49 +0100

    Update timeline

--- src/components/Timeline/timeline.jsx
+++ src/components/Timeline/timeline.jsx
@@ -5,56 +5,54 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import "./style.css";
+import TimelineComponent from "./timelineComponent";
+import TimelineText from "./TimeLineText.json";
 
 class TimeLine extends Component {
   render() {
     return (
       <div className="timeline-container">
         <VerticalTimeline>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: "rgb(62, 167, 6)", color: "#fff" }}
-            date="2010 - 2011"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          >
-            <h3 className="vertical-timeline-element-title">BKV Zrt.</h3>
-            <h4 className="vertical-timeline-element-subtitle">Budapest</h4>
-            <p>
-              Maintain, fixed problem on the Budapest metro line (CCTV,
-              Emergency Call, Switch)
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2010 - 2011"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          >
-            <h3 className="vertical-timeline-element-title">
-              Logiscool instructor
-            </h3>
-            <h4 className="vertical-timeline-element-subtitle">Vác</h4>
-            <p>Teaching children game developing.</p>
-          </VerticalTimelineElement>
-
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2010 - 2011"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          >
-            <h3 className="vertical-timeline-element-title">OtterWorks</h3>
-            <h4 className="vertical-timeline-element-subtitle">Budapest</h4>
-            <p>Develop game with Unity Engine.</p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-            date="2011 - present"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          >
-            <h3 className="vertical-timeline-element-title">Zyxel</h3>
-            <h4 className="vertical-timeline-element-subtitle">Budapest</h4>
-            <p>Configurate network devices.</p>
-          </VerticalTimelineElement>
+          <TimelineComponent
+            r="62"
+            g="167"
+            b="6"
+            textColor="#fff"
+            work={TimelineText.JobTitleOne}
+            workPlace={TimelineText.JobOneAddress}
+            date={TimelineText.JobOneDate}
+            desc={TimelineText.JobOneDescr}
+          />
+          <TimelineComponent
+            r="33"
+            g="150"
+            b="243"
+            textColor="#fff"
+            work={TimelineText.JobTitleTwo}
+            workPlace={TimelineText.JobTwoAddress}
+            date={TimelineText.JobTwoDate}
+            desc={TimelineText.JobTwoDescr}
+          />
+          <TimelineComponent
+            r="130"
+            g="14"
+            b="184"
+            textColor="#fff"
+            work={TimelineText.JobTitleThree}
+            workPlace={TimelineText.JobThreeAddress}
+            date={TimelineText.JobThreeDate}
+            desc={TimelineText.JobThreeDescr}
+          />
+          <TimelineComponent
+            r="14"
+            g="46"
+            b="185"
+            textColor="#fff"
+            work={TimelineText.JobTitleFour}
+            workPlace={TimelineText.JobFourAddress}
+            date={TimelineText.JobFourDate}
+            desc={TimelineText.JobFourDescr}
+          />
         </VerticalTimeline>
       </div>
     );
